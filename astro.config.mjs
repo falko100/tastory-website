bea1d165444309cd7eba153fb3fbee8082554d63
steijn-h <steijn@tastory.nl>
2026-03-10 10:37:11 +0100

    Fix formatting of site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.tastory.nl'
-
+  site: 'https://www.tastory.nl',
   integrations: [sitemap()],
   build: {
     assets: 'assets',
